68b291ae21b492908d3cf9f56a86f2d12cc8a890
Alex Pinto <alexfapa32@gmail.com>
2021-09-11 20:42:38 -0300

    Criação de projeto estudo React

---
+++ backend/src/test/java/com/fapadevelop/rdvendas/rdvendas/RdvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fapadevelop.rdvendas.rdvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RdvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
