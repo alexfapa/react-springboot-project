914cf6d6e495d09ba7508998e1faa0bee0d2f6f6
Alex Pinto <alexfapa32@gmail.com>
2021-09-14 00:50:58 -0300

    Configuração variável de ambiente

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,1 +1,6 @@
-export const BASE_URL='http://localhost:8080';
+export const BASE_URL= process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
+/* O ?? na linha acima indica um operador de coalecencia nula
+    Significa  que se a variável REACT_APP_BACKEND_URL não estiver definida, a aplicação pega o localhost
+    Resumindo: se rodar localmente pega o localhost, pois a variável REACT_APP_BACKEND_URL não está definida
+    Se rodar no netifly a variável está definida e portanto pega o link do back end definido lá.
+*/
